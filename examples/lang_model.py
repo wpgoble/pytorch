19e4a547b20b06fc0c7e41ab4f37db8cfe2abd47
William Goble <will.goble@gmail.com>
2022-11-29 18:58:24 -0500

    adds code snippet to load and batch the data

--- examples/lang_model.py
+++ examples/lang_model.py
@@ -7,6 +7,10 @@ import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from torch.utils.data import dataset
 
+from torchtext.datasets import WikiText2
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+
 class TransformerModel(nn.Module):
     def __init__(self, ntoken:int, d_model:int, nhead:int, d_hid:int, 
                     nlayers:int, dropout:float = 0.5):
@@ -70,3 +74,39 @@ class PositionalEncoding(nn.Module):
         """
         x = x + self.pe[:x.size(0)]
         return self.dropout(x)
+
+# Load and batch data
+train_iter = WikiText2(split='train')
+tokenizer = get_tokenizer('basic_english')
+vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>'])
+vocab.set_default_index(vocab['<unk>'])
+
+def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
+    """ Converts raw text into a float Tensor """
+    data = [torch.tensor(vocab(tokenizer(item)), d_type = torch.long) for item in raw_text_iter]
+    return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
+
+train_iter, val_iter, test_iter = WikiText2()
+train_data = data_process(train_iter)
+val_data = data_process(val_iter)
+test_data = data_process(test_iter)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def batchify(data:Tensor, bsz:int) -> Tensor:
+    """
+    Divides the data into bsz separate sequences, removing extra elements that 
+    wouldn't cleanly fit.
+
+    Args:
+        data: Tensor, shape [N]
+        bsz: int, batch size
+    
+    Returns:
+        Tensor of shape [N // bsz, bsz]
+    """
+
+    seq_len = data.size(0) // bsz
+    data = data[:seq_len * bsz]
+    data = data.view(bsz, seq_len).t().contiguous()
+    return data.to(device)
