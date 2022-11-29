12a56ea0f74dd4d2bc62bd239619f7dd7f58ad9c
William Goble <will.goble@gmail.com>
2022-11-29 18:37:23 -0500

    adds TransformerModel

--- examples/lang_model.py
+++ examples/lang_model.py
@@ -7,6 +7,39 @@ import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from torch.utils.data import dataset
 
+class TransformerModel(nn.Module):
+    def __init__(self, ntoken:int, d_model:int, nhead:int, d_hid:int, 
+                    nlayers:int, dropout:float = 0.5):
+        super().__init__()
+        self.model_type = "Transformer"
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        self.encoder = nn.Embedding(ntoken, d_model)
+        self.d_model = d_model
+        self.decoder = nn.Linear(d_model, ntoken)
+
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+    
+    def forward(self, src:Tensor, src_mask:Tensor) -> Tensor:
+        """
+        Args:
+            src: Tensor, shape [seq_length, batch_size]
+            src_mask: Tensor, shape [seq_length, seq_length]
+        
+        Returns:
+            output Tensor of shape [seq_length, batch_size, ntoken]
+        """
+        src = self.encoder(src) * math.sqrt(self.d_model)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, src_mask)
+        output = self.decoder(output)
+        return output
+
 """
 PositionalEncoding module injects some information about the relative or 
 absolute position of the tokens in the sequence. The positional encodings have 
